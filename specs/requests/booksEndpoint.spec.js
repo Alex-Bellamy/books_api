6686b2217de1bb43ccf5bcabc27a14f89c0c124b
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-26 12:18:16 +0200

    adding response and list of books tests

--- specs/requests/booksEndpoint.spec.js
+++ specs/requests/booksEndpoint.spec.js
@@ -12,3 +12,20 @@ before((done) => {
 after((done) => {
   server.close(done);
 });
+
+describe("GET /api/v1/books", () => {
+  before(async () => {
+    response = await request.get("/api/v1/books");
+  });
+
+  it("is expected to respond with status 200", () => {
+    expect(response.status).to.equal(200);
+  });
+
+  it("is expected to respond with list of books", async () => {
+    const expectedResponse = {
+      books: [{ title: "Alex life" }],
+    };
+    expect(response.body).to.deep.equal(expectedResponse);
+  });
+});
