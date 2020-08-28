6d8f8976402573d47db18cf93efb26c9a44e66fa
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-28 11:00:32 +0200

    setting up factories and refactoring specs

--- specs/requests/booksEndpoint.spec.js
+++ specs/requests/booksEndpoint.spec.js
@@ -1,6 +1,7 @@
 const app = require("../../app");
 const supertest = require("supertest");
 const expect = require("chai").expect;
+const { factory } = require("../helpers");
 
 let server, request, response;
 
@@ -13,8 +14,19 @@ after((done) => {
   server.close(done);
 });
 
+beforeEach(async () => {
+  await factory.createMany("Book", 2, [
+    ({ id: 100, title: "This is a factory title" },
+    { id: 101, title: "Learn Sequelize with Adi - in test" }),
+  ]);
+});
+
+afterEach(async () => {
+  await factory.cleanUp();
+});
+
 describe("GET /api/v1/books", () => {
-  before(async () => {
+  beforeEach(async () => {
     response = await request.get("/api/v1/books");
   });
 
@@ -23,13 +35,15 @@ describe("GET /api/v1/books", () => {
   });
 
   it("is expected to respond with two books", () => {
-    expect(response.body['books'].length).to.equal(2);
+    expect(response.body["books"].length).to.equal(2);
   });
 });
 
-describe('GET /api/v1/books/:id', () => {
-  it('is expected to respond with a single book', async () => {
-    response = await request.get('/api/v1/books/3')
-    expect(response.body.book.title).to.equal('Learn Sequelize with Adi - in test')
+describe("GET /api/v1/books/:id", () => {
+  it("is expected to respond with a single book", async () => {
+    response = await request.get("/api/v1/books/101");
+    expect(response.body.book.title).to.equal(
+      "Learn Sequelize with Adi - in test"
+    );
   });
 });
